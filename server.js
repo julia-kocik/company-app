36e9b6353f1d91fe6c608c75ed74f4fc1547ff47
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-29 12:02:55 +0200

    Refactor endpoints

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
 mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err){
+  if(err) {
     console.log(err);
   }
   else {
     console.log('Successfully connected to the database');
+
     const db = client.db('companyDB');
     const app = express();
 
@@ -19,37 +20,21 @@ mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUni
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
+
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
     app.use('/api', productsRoutes);
 
     app.use((req, res) => {
       res.status(404).send({ message: 'Not found...' });
-    })
+    });
 
     app.listen('8000', () => {
       console.log('Server is running on port: 8000');
     });
-
-    db.collection('employees').find({ department: 'IT' }, (err, data) => {
-      if(!err) {
-        data.each((error, employee) => {
-          console.log(employee);
-        })
-      }
-    });
-
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      if(err) console.log('err');
-    });
-
-    db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 }}, err => {
-      if(err) console.log(err);
-    });
-
-    db.collection('departments').deleteOne({ name: 'Management' }, (err) => {
-      if(err) console.log(err);
-    });
-    
   }
 });
