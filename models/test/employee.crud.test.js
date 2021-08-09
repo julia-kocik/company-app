fca4a6437ae029a2b255eda77e3981f78b36353b
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-08-09 13:35:53 +0200

    Fix employee CRUD tests

--- models/test/employee.crud.test.js
+++ models/test/employee.crud.test.js
@@ -26,10 +26,10 @@ describe('Employee', () => {
 
 describe("Reading data", () => {
   before(async () => {
-      const testEmpOne = new Employee({ name: "Employee #1" });
+      const testEmpOne = new Employee({ firstName: "John", lastName: 'Doe', department: 'IT' });
       await testEmpOne.save();
   
-      const testEmpTwo = new Employee({ name: "Employee #2" });
+      const testEmpTwo = new Employee({ firstName: "Amanda", lastName: 'Doe', department: 'Marketing' });
       await testEmpTwo.save();
   });
   it('should return all the data with "find" method', async () => {
@@ -38,9 +38,9 @@ describe("Reading data", () => {
     expect(employees.length).to.be.equal(expectedLength);
   });
   it('should return a proper document by "name" with "findOne" method', async () => {
-    const employee = await Employee.findOne({ name: 'Employee #1' });
-    const expectedName = 'Employee #1';
-    expect(employee.name).to.be.equal(expectedName);
+    const employee = await Employee.findOne({  firstName: "John" });
+    const expectedName = 'John';
+    expect(employee.firstName).to.be.equal(expectedName);
   });
   after(async () => {
     await Employee.deleteMany();
@@ -51,7 +51,7 @@ describe("Reading data", () => {
 describe('Creating data', () => {
 
     it('should insert new document with "insertOne" method', async () => {
-        const employee = new Employee({ name: 'Employee #1' });
+        const employee = new Employee({  firstName: "John" });
         await employee.save();
         expect(employee.isNew).to.be.false;
     });
@@ -63,31 +63,31 @@ describe('Creating data', () => {
 
 describe('Updating data', () => {
     beforeEach(async () => {
-        const testEmpOne = new Employee({ name: 'Employee #1' });
+        const testEmpOne = new Employee({  firstName: "John"});
         await testEmpOne.save();
       
-        const testEmpTwo = new Employee({ name: 'Employee #2' });
+        const testEmpTwo = new Employee({  firstName: "Amanda" });
         await testEmpTwo.save();
     });
 
     it('should properly update one document with "updateOne" method', async () => {
-        await Employee.updateOne({ name: 'Employee #1' }, { $set: { name: '=Employee #1=' }});
-        const updatedEmployee = await Employee.findOne({ name: '=Employee #1=' });
+        await Employee.updateOne({ firstName: 'John' }, { $set: { firstName: 'Amanda' }});
+        const updatedEmployee = await Employee.findOne({ firstName: 'Amanda' });
         expect(updatedEmployee).to.not.be.null;
     });
   
     it('should properly update one document with "save" method', async () => {
-        const employee = await Employee.findOne({ name: 'Employee #1' });
-        employee.name = '=Emloyee #1=';
+        const employee = await Employee.findOne({ firstName: 'Amanda' });
+        employee.name = 'John';
         await employee.save();
       
-        const updatedEmployee = await Employee.findOne({ name: '=Employee #1=' });
+        const updatedEmployee = await Employee.findOne({ firstName: 'John' });
         expect(updatedEmployee).to.not.be.null;
     });
   
     it('should properly update multiple documents with "updateMany" method', async () => {
-        await Employee.updateMany({}, { $set: { name: 'Updated!' }});
-        const employees = await Employee.find({ name: 'Updated!' });
+        await Employee.updateMany({}, { $set: { firstName: 'Updated!' }});
+        const employees = await Employee.find({ firstName: 'Updated!' });
         expect(employees.length).to.be.equal(2);
     });  
     
@@ -98,23 +98,23 @@ describe('Updating data', () => {
 
   describe('Removing data', () => {
     beforeEach(async () => {
-        const testEmpOne = new Employee({ name: 'Employee #1' });
+        const testEmpOne = new Employee({ firstName: 'John' });
         await testEmpOne.save();
       
-        const testEmpTwo = new Employee({ name: 'Employee #2' });
+        const testEmpTwo = new Employee({ firstName: 'Amanda' });
         await testEmpTwo.save();
       });
 
     it('should properly remove one document with "deleteOne" method', async () => {
-        await Employee.deleteOne({ name: 'Employee #1' });
-        const removeEmployee = await Employee.findOne({ name: 'Employee #1' });
+        await Employee.deleteOne({ firstName: 'John' });
+        const removeEmployee = await Employee.findOne({ firstName: 'John' });
         expect(removeEmployee).to.be.null;
     });
   
     it('should properly remove one document with "remove" method', async () => {
-        const employee = await Employee.findOne({ name: 'Employee #1' });
+        const employee = await Employee.findOne({ firstName: 'John' });
         await employee.remove();
-        const removedEmployee = await Employee.findOne({ name: 'Employee #1' });
+        const removedEmployee = await Employee.findOne({ firstName: 'John' });
         expect(removedEmployee).to.be.null;
     });
   
